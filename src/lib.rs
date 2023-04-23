54821b2e02268ef3fcb24c717cab736b86790825
tiger-chan <anthony.r.young@gmail.com>
2023-04-23 11:23:17 -0600

    Add prelude

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,10 @@ pub mod float;
 pub mod math;
 pub mod source;
 mod task;
+
+pub mod prelude {
+    pub use super::float::Float;
+    pub use super::math;
+    pub use super::source::{Gradient, Noise, Perlin};
+    pub use super::task::*;
+}
