de92469ee825e1c30a7023b4d6183748d6c0b9fe
Anthony Young <anthony.r.young@gmail.com>
2024-01-04 22:13:12 -0700

    Attempt to normalize the cells (vec2 only at the moment)

--- src/source/cellular.rs
+++ src/source/cellular.rs
@@ -15,6 +15,125 @@ pub enum Distance {
     Manhattan,
 }
 
+pub trait Sqrt {
+    fn sqrt(self) -> Self;
+}
+
+impl Sqrt for f32 {
+    fn sqrt(self) -> Self {
+        f32::sqrt(self)
+    }
+}
+
+impl Sqrt for f64 {
+    fn sqrt(self) -> Self {
+        f64::sqrt(self)
+    }
+}
+
+pub struct Vec2<T>(pub T, pub T);
+
+impl<T: Copy + Clone> Clone for Vec2<T> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+impl<T: Copy> Copy for Vec2<T> {}
+
+impl<T> Vec2<T> {
+    pub fn new(x: T, y: T) -> Self {
+        Self(x, y)
+    }
+}
+
+impl<T> std::ops::Add for Vec2<T>
+where
+    T: std::ops::Add<Output = T>,
+{
+    type Output = Vec2<T>;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
+
+impl<T> std::ops::Sub for Vec2<T>
+where
+    T: std::ops::Sub<Output = T>,
+{
+    type Output = Vec2<T>;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self(self.0 - rhs.0, self.1 - rhs.1)
+    }
+}
+
+impl<T> std::ops::Div<T> for Vec2<T>
+where
+    T: std::ops::Div<Output = T> + Copy,
+{
+    type Output = Vec2<T>;
+    fn div(self, rhs: T) -> Self::Output {
+        Self(self.0 / rhs, self.1 / rhs)
+    }
+}
+
+impl<T> std::ops::Mul<T> for Vec2<T>
+where
+    T: std::ops::Mul<Output = T> + Copy,
+{
+    type Output = Vec2<T>;
+    fn mul(self, rhs: T) -> Self::Output {
+        Self(self.0 * rhs, self.1 * rhs)
+    }
+}
+
+pub struct Vec3<T>(pub T, pub T, pub T);
+
+impl<T> Vec3<T> {
+    pub fn new(x: T, y: T, z: T) -> Self {
+        Self(x, y, z)
+    }
+}
+
+impl<T> std::ops::Add for Vec3<T>
+where
+    T: std::ops::Add<Output = T>,
+{
+    type Output = Vec3<T>;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self(self.0 + rhs.0, self.1 + rhs.1, self.2 + rhs.2)
+    }
+}
+
+impl<T> std::ops::Sub for Vec3<T>
+where
+    T: std::ops::Sub<Output = T>,
+{
+    type Output = Vec3<T>;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self(self.0 - rhs.0, self.1 - rhs.1, self.2 - rhs.2)
+    }
+}
+
+impl<T> std::ops::Div<T> for Vec3<T>
+where
+    T: std::ops::Div<Output = T> + Copy,
+{
+    type Output = Vec3<T>;
+    fn div(self, rhs: T) -> Self::Output {
+        Self(self.0 / rhs, self.1 / rhs, self.2 / rhs)
+    }
+}
+
+impl<T> std::ops::Mul<T> for Vec3<T>
+where
+    T: std::ops::Mul<Output = T> + Copy,
+{
+    type Output = Vec3<T>;
+    fn mul(self, rhs: T) -> Self::Output {
+        Self(self.0 * rhs, self.1 * rhs, self.2 * rhs)
+    }
+}
+
 const PRIME_X: u64 = 2053;
 const PRIME_Y: u64 = 7177;
 const PRIME_Z: u64 = 4943;
@@ -29,11 +148,11 @@ macro_rules! cellular {
         }
 
         fn manhattan_xy(dx: $T, dy: $T) -> $T {
-            (dx.abs() + dy.abs())
+            dx.abs() + dy.abs()
         }
 
         fn manhattan_xyz(dx: $T, dy: $T, dz: $T) -> $T {
-            (dx.abs() + dy.abs() + dz.abs())
+            dx.abs() + dy.abs() + dz.abs()
         }
 
         fn euclidean_x(dx: $T) -> $T {
@@ -90,16 +209,16 @@ macro_rules! cellular {
                 rng.next()
             }
 
-            fn offset_xy(&self, ix: i32, iy: i32) -> ($T, $T) {
+            fn offset_xy(&self, ix: i32, iy: i32) -> Vec2 {
                 let seed = self.seed_from_xy(ix, iy);
                 let mut rng = PCG::new(seed);
-                (rng.next(), rng.next())
+                Vec2::new(rng.next(), rng.next())
             }
 
-            fn offset_xyz(&self, ix: i32, iy: i32, iz: i32) -> ($T, $T, $T) {
+            fn offset_xyz(&self, ix: i32, iy: i32, iz: i32) -> Vec3 {
                 let seed = self.seed_from_xyz(ix, iy, iz);
                 let mut rng = PCG::new(seed);
-                (rng.next(), rng.next(), rng.next())
+                Vec3::new(rng.next(), rng.next(), rng.next())
             }
 
             fn dist_x(&self, x: $T) -> $T {
@@ -137,14 +256,23 @@ macro_rules! cellular {
                 let ix = lx.floor() as i32;
                 let fx = lx.fract();
 
+                const CELLS: [i32; 2] = [-1, 1];
+
+                let origin = self.offset_x(ix);
+                let mut end = origin;
                 let mut min_dist: $T = 10.0;
-                for dx in -1..=1 {
+                for dx in CELLS {
                     let nfx = dx as $T + self.offset_x(ix + dx);
                     let dist = self.dist_x(nfx - fx);
-                    min_dist = min_dist.min(dist);
+                    if dist < min_dist {
+                        end = nfx;
+                        min_dist = dist;
+                    }
                 }
 
-                min_dist * 2.0 - 1.0
+                let a = (fx - origin) / (end - origin);
+                let a = if a > 0.5 { a - 0.5 } else { a };
+                a * 2.0
             }
 
             /// Calculates the dot product of the x, y values scaled to the range [-1, 1].
@@ -158,18 +286,39 @@ macro_rules! cellular {
                 let fx = lx.fract();
                 let fy = ly.fract();
 
-                let mut min_dist: $T = 10.0;
-                for dx in -1..=1 {
-                    for dy in -1..=1 {
-                        let (nx, ny) = self.offset_xy(ix + dx, iy + dy);
-                        let dfx = (dx as $T + nx) - fx;
-                        let dfy = (dy as $T + ny) - fy;
-                        let dist = self.dist_xy(dfx, dfy);
-                        min_dist = min_dist.min(dist);
+                #[rustfmt::skip]
+                        const CELLS: [(i32, i32); 9] = [
+                            (-1, -1), (-1, 0), (-1, 1),
+                            (0, -1), (0,0), (0, 1),
+                            (1, -1), (1, 0), (1, 1),
+                        ];
+
+                // Define an array to store distances to the nearest and second-nearest points
+                let mut distances: [$T; 2] = [10.0, 10.0];
+                for (dx, dy) in CELLS {
+                    let n = self.offset_xy(ix + dx, iy + dy);
+                    let dfx = (dx as $T + n.0) - fx;
+                    let dfy = (dy as $T + n.1) - fy;
+                    let dist = self.dist_xy(dfx, dfy);
+
+                    // Update distances array
+                    if dist < distances[0] {
+                        distances.swap(0, 1);
+                        distances[0] = dist;
+                    } else if dist < distances[1] {
+                        distances[1] = dist;
                     }
                 }
 
-                min_dist * 2.0 - 1.0
+                // Calculate normalized difference between distances
+                let noise_value = if distances[1] != 0.0 {
+                    (distances[0] - distances[1]) / distances[1]
+                } else {
+                    0.0
+                };
+
+                // Return the normalized noise value
+                noise_value
             }
 
             /// Calculates the dot product of the x, y, and z values scaled to the range [-1, 1].
@@ -190,10 +339,10 @@ macro_rules! cellular {
                 for dx in -1..=1 {
                     for dy in -1..=1 {
                         for dz in -1..=1 {
-                            let (nx, ny, nz) = self.offset_xyz(ix + dx, iy + dy, iz + dz);
-                            let dfx = (dx as $T + nx) - fx;
-                            let dfy = (dy as $T + ny) - fy;
-                            let dfz = (dz as $T + nz) - fz;
+                            let n = self.offset_xyz(ix + dx, iy + dy, iz + dz);
+                            let dfx = (dx as $T + n.0) - fx;
+                            let dfy = (dy as $T + n.1) - fy;
+                            let dfz = (dz as $T + n.2) - fz;
                             let dist = self.dist_xyz(dfx, dfy, dfz);
                             min_dist = min_dist.min(dist);
                         }
@@ -211,6 +360,9 @@ pub mod f32 {
     use super::{PRIME_X, PRIME_Y, PRIME_Z, XOR_X, XOR_Y, XOR_Z};
     use crate::rng::PCG;
     use crate::source::f32::Noise;
+
+    type Vec2 = super::Vec2<f32>;
+    type Vec3 = super::Vec3<f32>;
 
     cellular!(f32);
 
@@ -232,6 +384,9 @@ pub mod f64 {
     use super::{PRIME_X, PRIME_Y, PRIME_Z, XOR_X, XOR_Y, XOR_Z};
     use crate::rng::PCG;
     use crate::source::f64::Noise;
+
+    type Vec2 = super::Vec2<f64>;
+    type Vec3 = super::Vec3<f64>;
 
     cellular!(f64);
 }
